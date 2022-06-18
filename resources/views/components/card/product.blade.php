e6c02de2ef3478e30176ea62a3ea82145f5c0abf
MuhamedKhX <muhamedkhx2@gmail.com>
2022-06-18 21:01:21 +0200

    fix

--- resources/views/components/card/product.blade.php
+++ resources/views/components/card/product.blade.php
@@ -7,10 +7,17 @@
         <div class="card-body ps-0 bg-200">
             <h5 class="fw-bold text-1000 text-truncate {{ arRight() }}">{{ $name }}</h5>
             <div class="fw-bold {{ arRight() }}">
-                @if($oldPrice)
-                    <span class="me-2 text-danger text-decoration-line-through">{{ $oldPrice }}</span>
+                @if(ar())
+                    <span class="text-800 ">{{ $price }}</span>
+                    @if($oldPrice)
+                        <span style="color: #ab0000" class="me-2 text-decoration-line-through">{{ $oldPrice }}</span>
+                    @endif
+                @else
+                    @if($oldPrice)
+                        <span class="me-2 text-danger text-decoration-line-through">{{ $oldPrice }}</span>
+                    @endif
+                    <span class="text-800 ">{{ $price }}</span>
                 @endif
-                <span class="text-800 ">{{ $price }}</span>
             </div>
         </div>
         {{ isset($link) ? $link : '' }}
