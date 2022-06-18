112da4ea46389564a6e9a7fcdce5d2acbea3a889
MuhamedKhX <muhamedkhx2@gmail.com>
2022-06-18 19:55:18 +0200

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,44 +31,6 @@ class DatabaseSeeder extends Seeder
         ]);
         $this->call(CollectionSeeder::class);
         $this->call(CitySeeder::class);
-
-        $category = Category::factory()->create();
-        $clothes = [/*
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Tisort/5642651/2300495',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/5677110/2379584',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/5840056/2372280',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Pantolon/5889619/2385446',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/5152739/1677974',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/4966620/1570095',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/4966623/1572076',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/5387674/2381092',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/outlet/LC-WAIKIKI/erkek/Gomlek/5175428/1657014',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/5840074/2390467',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/4960717/1575237',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/outlet/LC-WAIKIKI/erkek/Pantolon/5103498/1680990',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Sort/5665796/2374427',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/5840074/2376966',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/XSIDE/erkek/Gomlek/5786224/2368106',*/
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/5731208/2414786',
-            'https://www.lcwaikiki.com/tr-TR/TR/urun/LC-WAIKIKI/erkek/Gomlek/5708750/2328983'
-        ];
-
-        foreach ($clothes as $clothe) {
-            $product = new Product();
-            $product->websiteScraper = 'LC';
-            $product->url            = $clothe;
-            $product->category_id    = $category->id;
-            $product->user_id        = $user->id;
-            $product->name           = 'TShirt';
-            $product->description    = 'lorem ispum';
-            $product->save();
-
-            $this->scrapColors(
-                productId: $product->id,
-                uri: $product->url
-            );
-        }
-
     }
 
     public function scrapColors($productId, $uri)
